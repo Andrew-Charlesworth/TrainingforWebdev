6954550f0828bad9fd0b7330bdc2464ef0aafe03
andrew-charlesworth <charlesworth_andrew@yahoo.com>
2019-06-21 16:42:25 -0600

    6212019f

---
+++ portfolio/loggingLetters.js
@@ -0,0 +1,15 @@
+/*
+ Name: Andrew Charlesworth
+Date Created:6/21/2019
+Most Recent Revision:
+*/
+
+
+
+var sentence = "I love learning software developement";
+
+for (var i=0; i < sentence.length; i++) {
+	if(sentence[i] !== "") {
+		console.log(sentence[i]);
+	}
+}
